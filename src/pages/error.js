a9e7b84c526f2e7eeb505adf36704891a09f4b26
Sholastix <drenai6436@gmail.com>
2022-11-21 18:04:07 +0200

    'error' page styling added.

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,5 +1,7 @@
+import styles from './error.module.css';
+
 const Error = () => {
-  return <h1>404 - PAGE NOT FOUND</h1>
+  return <h1 className={styles.error}>404 - PAGE NOT FOUND</h1>
 };
 
 export default Error;
