c50f7a6748c9f940b7c47ade97bc5efc9b9997d6
Sholastix <drenai6436@gmail.com>
2022-11-22 16:12:21 +0200

    Firebase configuration file added.

---
+++ src/config/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration:
+const firebaseConfig = {
+  apiKey: "AIzaSyABIWdQVdb-lEwza-63MIbZPduiCsNxX58",
+  authDomain: "chitchat-bfdb1.firebaseapp.com",
+  projectId: "chitchat-bfdb1",
+  storageBucket: "chitchat-bfdb1.appspot.com",
+  messagingSenderId: "326016380321",
+  appId: "1:326016380321:web:2b02dd50bc6af900b52fe0"
+};
+
+
+// Initialize Firebase:
+const app = initializeApp(firebaseConfig);
