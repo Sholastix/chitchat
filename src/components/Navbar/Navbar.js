df950aefcb644d8b55ca9b19e9ad2d5b9004e0ca
Sholastix <drenai6436@gmail.com>
2022-11-25 18:00:07 +0200

    'Navbar.js': hooks library for better authorization added, logout button temporarly added.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
-import { auth } from '../../config/firebase';
+// Hook for authomatic logout from current account and login with new account.
+import { useAuthState } from 'react-firebase-hooks/auth';
 
+import { auth } from '../../config/firebase';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
+  // 'useAuthState()' hook automatically updates 'user' variable, when we try to login with new account.
+  const [user] = useAuthState(auth);
+
   return (
     <div>
-      <Link to='/'>Main</Link>
-      <Link to='/login'>Login</Link>
-
+      <div className='links'>
+        <Link to='/'>Main</Link>
+        <Link to='/login'>Login</Link>
+      </div>
+      <div className='user'>
+        <p>{user?.displayName}</p>
+        <img src={user?.photoURL} />
+      </div>
       <div>
-        <p>{auth.currentUser?.displayName}</p>
-        <img src={auth.currentUser?.photoURL} />
+        <button onClick={() => auth.signOut()}>LogOut</button>
       </div>
     </div>
   )
