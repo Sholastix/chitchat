593bac4f56921398975e7007512c08dfc37eb627
Sholastix <drenai6436@gmail.com>
2022-12-03 20:06:54 +0200

    'Post.js': added method for adding likes to posts.

--- src/pages/main/post.js
+++ src/pages/main/post.js
@@ -1,13 +1,34 @@
+import { addDoc, collection } from 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import { auth, db } from '../../config/firebase';
+
 const Post = (props) => {
   const { post } = props;
+  const [user] = useAuthState(auth);
+  // Creating reference to our 'likes' collection.
+  const likesRef = collection(db, 'likes');
+
+  const createLike = async () => {
+    await addDoc(likesRef, {
+      postId: post.id,
+      userId: user?.uid,
+    });
+  };
 
   return (
     <div>
-      <div><h3>{post.title}</h3></div>
-      <div>{post.description}</div>
-      <div>@{post.username}</div>
-      <button> &#x1F44D; </button>
-      <p>Likes:</p>
+      <div>
+        <h3>{post.title}</h3>
+      </div>
+      <div>
+        <p>{post.description}</p>
+      </div>
+      <div>
+        <p>@{post.username}</p>
+        <button onClick={() => { createLike() }}> &#x1F44D; </button>
+        <p>Likes:</p>
+      </div>
     </div>
   )
 };
