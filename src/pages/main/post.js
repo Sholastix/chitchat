c0b0ec1cf60e1578d8c524cbf89cc66dacd94245
Sholastix <drenai6436@gmail.com>
2022-12-04 17:30:03 +0200

    'Post.js': 'likes' counter in UI has been implemented.

--- src/pages/main/post.js
+++ src/pages/main/post.js
@@ -1,19 +1,37 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { addDoc, getDocs, collection, query, where } from 'firebase/firestore';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { auth, db } from '../../config/firebase';
 
 const Post = (props) => {
   const { post } = props;
+
+  useEffect(() => {
+    getLikes();
+  }, []);
+
+  const [likes, setLikes] = useState(null);
+
   const [user] = useAuthState(auth);
   // Creating reference to our 'likes' collection.
   const likesRef = collection(db, 'likes');
+  // Creating a query to database with help of native Firestore method 'where()'.
+  // Here we specifying for docs which we want to retrieve from database.
+  const likesDoc = query(likesRef, where('postId', '==', post.id))
+
+  const getLikes = async () => {
+    const result = await getDocs(likesDoc);
+    setLikes(result.docs.length);
+  };
 
   const createLike = async () => {
     await addDoc(likesRef, {
       postId: post.id,
       userId: user?.uid,
     });
+
+    setLikes(likes + 1);
   };
 
   return (
@@ -27,7 +45,7 @@ const Post = (props) => {
       <div>
         <p>@{post.username}</p>
         <button onClick={() => { createLike() }}> &#x1F44D; </button>
-        <p>Likes:</p>
+        {likes && <p>Likes: {likes}</p>}
       </div>
     </div>
   )
